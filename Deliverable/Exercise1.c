04d1ef153d970760f241a1aecd28f462a5620ce3
Paschalina Matziarli <pascmatz@mbg.duth.gr>
2025-11-24 20:43:09 +0100

    Add files via upload

---
+++ Deliverable/Exercise1.c
@@ -0,0 +1,64 @@
+#include <stdio.h>
+#include <string.h>
+
+int main()
+
+{
+
+	char seq [ 100000 ];
+	int len, i, j ;
+	int valid_cr;
+
+	printf( "Give the sequences: \n" );
+
+
+	while ( scanf( "%s", seq) ==  1)
+
+	{
+		len = strlen( seq );
+		valid_cr = 0;
+
+		// First check length, we need at least 6 nucleotides
+		if ( len <6 )
+			{
+				printf("Invalid data! Sequence is too short.\n");
+				continue;
+			}
+
+
+		// Check for start codon
+		for ( i = 0 ; i <= len-3 ; i++ )
+
+			{
+				if ( seq [ i ] == 'A' && seq [ i+1 ] == 'T' && seq [ i+2 ] == 'G')
+					{
+						// Check for stop codon in the same reading frame
+						for ( j = i+3 ; j <= len-3 ; j += 3 )
+
+							{
+								if (( seq [ j ] == 'T' && seq [ j+1 ] == 'A' && seq [ j+2 ] == 'A') ||
+								( seq [ j ] == 'T' && seq [ j+1 ] == 'A' && seq [ j+2 ] == 'G') ||
+								( seq [ j ] == 'T' && seq [ j+1 ] == 'G' && seq [ j+2 ] == 'A'))
+									{
+										printf( "Start codon: %d, End codon: %d, Length: %d.\n", i+1, j+1, (j-i) +3 );
+										valid_cr = 1;
+										i= j+1;
+										break;
+									}
+
+							}
+
+					}
+
+			}
+
+		if (!valid_cr)
+			{
+				printf("Invalid coding region.\n");
+			}
+
+	}
+
+	return 0;
+
+}
